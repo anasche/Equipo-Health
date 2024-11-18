4943d28ec74becfbe39199705dc61b0042e7f6f4
anas <anas.m@webandcrafts.com>
2024-11-18 09:00:02 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@
 import "./customBootstrap.scss";
 
 import React from "react";
-import Header from "./components/Header";
-import TrustedBySection from "./widgets/TrustedBySection";
-import FeaturesSection from "./widgets/FeaturesSection";
-import PricingTable from "./widgets/PricingTable";
+import Header from "./components/Header/header";
+import TrustedBySection from "./widgets/TrustedBySection/trustedBySection";
+import FeaturesSection from "./widgets/FeaturesSection/featuresSection";
+import PricingTable from "./widgets/PricingTable/pricingTable";
 import HeroSection from "./widgets/HeroSection";
-import EaseInHand from "./widgets/EaseInHand";
+import EaseInHand from "./widgets/EaseInHand/easeInHand";
 import FAQ from "./widgets/Faq";
 import Footer from "./components/Footer";
 
